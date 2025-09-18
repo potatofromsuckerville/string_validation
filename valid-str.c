2c76e9452c841ba9495accf640a6fec3b82ed76a
Awwal <awwalonpc@gmail.com>
2025-09-18 22:12:15 +0100

    Modularised the program to make it more flexible.

--- valid-str.c
+++ valid-str.c
@@ -1,38 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int get_input(char* buffer, size_t buff_size) {
 	
-	char input[7];
-	printf("==== INPUT VALIDATION (PERSONAL BUILD V1) ==== \n");
-	printf("Enter %zu characters max: \n", sizeof(input) - 1);
-	fgets(input, sizeof(input), stdin);
-	
+	fgets(buffer, buff_size, stdin);
 	int stringLength; 
-	stringLength = strlen(input);
+	stringLength = strlen(buffer);
 	
 	if (stringLength == 1) {
-		printf("Empty input \n");
+		return 2;
 		}
 	
-	else if (input[stringLength - 1] == '\n') { // i.e. less than max
-		input[stringLength - 1] = '\0';
-		printf("Valid input (less than max). You entered: %s \n", input);
+	else if (buffer[stringLength - 1] == '\n') { // i.e. less than max
+		buffer[stringLength - 1] = '\0';
+		return 1;
 		}
 			
 	else {
 		if (getchar() == '\n') {
-			printf("Valid input(exactly max). You entered: %s \n", input);
+			return 1;
 			}
 		else {
 			int c;
 			while ((c = getchar()) != '\n' && c != EOF) {
 				}
-			printf("Invalid input. Truncation occured. \n");
+			return 0;
 			}	
-		}		
-	
-	return 0;
-	
+		}
 	}
 
+int main() {
+	
+	char input[7];
+	printf("==== INPUT VALIDATION (PERSONAL BUILD V1) ==== \n");
+	printf("Enter %zu characters max: \n", sizeof(input) - 1);
+	int state = get_input(input, sizeof(input));
+	
+	switch (state) {
+		case 0:
+			printf("Warning: Buffer overflow. Truncation ocurred. \n");
+			printf("You entered: %s \n", input);
+			break;
+			
+		case 1:
+			printf("Full input received. You entered: %s \n", input);
+			break;
+			
+		case 2:
+			printf("Empty input. Program terminated. \n");
+			break;
+			
+		default:
+			printf("Error reading input. Proogram terminated/ \n");
+			break;
+		}
+	
+	return 0;
+	}
