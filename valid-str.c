8479cfc83bab86d5d1b4e92988c1d0d3419d83f2
Awwal <awwalonpc@gmail.com>
2025-09-19 04:31:52 +0100

    Cleaned up the code some more. Used custom constants for return values to improbe readability. Also added EOF handling to allow for proper input redirection.

--- valid-str.c
+++ valid-str.c
@@ -1,30 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+#define INPUT_ERROR -1
+#define INPUT_TRUNCATED 0
+#define INPUT_VALID 1
+#define INPUT_EMPTY 2
+
 int get_input(char* buffer, size_t buff_size) {
 	
-	fgets(buffer, buff_size, stdin);
+	if (fgets(buffer, buff_size, stdin) == NULL) {
+		return INPUT_ERROR;
+		}
+		
 	int stringLength; 
 	stringLength = strlen(buffer);
 	
 	if (stringLength == 1) {
-		return 2;
+		return INPUT_EMPTY;
 		}
 	
 	else if (buffer[stringLength - 1] == '\n') { // i.e. less than max
 		buffer[stringLength - 1] = '\0';
-		return 1;
+		return INPUT_VALID;
 		}
 			
 	else {
 		if (getchar() == '\n') {
-			return 1;
+			return INPUT_VALID;
 			}
 		else {
 			int c;
 			while ((c = getchar()) != '\n' && c != EOF) {
 				}
-			return 0;
+			return INPUT_TRUNCATED;
 			}	
 		}
 	}
@@ -37,21 +45,24 @@ int main() {
 	int state = get_input(input, sizeof(input));
 	
 	switch (state) {
-		case 0:
-			printf("Warning: Buffer overflow. Truncation ocurred. \n");
-			printf("You entered: %s \n", input);
+		case INPUT_ERROR:
+			printf("Error reading input. Program terminated \n");
+			break;		
+		
+		case INPUT_TRUNCATED:
+			printf("Warning: Buffer overflow. Input truncated to \"%s\". \n", input);
 			break;
 			
-		case 1:
+		case INPUT_VALID:
 			printf("Full input received. You entered: %s \n", input);
 			break;
 			
-		case 2:
+		case INPUT_EMPTY:
 			printf("Empty input. Program terminated. \n");
 			break;
 			
 		default:
-			printf("Error reading input. Proogram terminated/ \n");
+			printf("Unknown Error. Exiting program. \n");
 			break;
 		}
 	
